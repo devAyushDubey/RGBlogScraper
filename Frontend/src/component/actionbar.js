493e6cc869ee853bba1d0ab8ec89b85de8135885
anju2084 <anju2084@.com>
2023-11-26 04:22:58 +0530

    downloading csv

--- Frontend/src/component/actionbar.js
+++ Frontend/src/component/actionbar.js
@@ -1,3 +1,4 @@
+"use client";
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -6,12 +7,35 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import XLSXDownload from '../component/downloadxls'
+
 export default function ButtonAppBar() {
-  
-    return (
+  const handleFileDownload = async () => {
+    try {
+      
+      const fileUrl = 'https://github.com/soumenkp2/DataHarvest---RateGain/blob/main/web_scraping/blogs_data.csv';
+      const rawUrl = 'https://raw.githack.com${fileUrl.slice(18)}';
+      const response = await fetch(rawUrl);
+      const data = await response.blob();
+
+      // Create a link element and trigger a click on it to initiate the download
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(data);
+
+      // Extract the filename from the URL or use a default name
+      const filename = fileUrl.substring(fileUrl.lastIndexOf('/') + 1) || 'downloadedFile';
+
+      link.download = filename;
+      link.click();
+
+      URL.revokeObjectURL(link.href);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ background: 'var(--primary-color)' }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -25,7 +49,14 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             RGBlogScraper
           </Typography>
-          <Button onClick={XLSXDownload} color="inherit">Download xls</Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleFileDownload}
+            sx={{ backgroundColor: 'var(--button-bg-color)', color: 'var(--button-text-color)' }}
+          >
+            Download XLS
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
--- Frontend/src/component/downloadxls.js
+++ Frontend/src/component/downloadxls.js
@@ -1,56 +1,50 @@
 "use client";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 
-class XLSXDownload extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-    };
-  }
+const XLSXDownload = () => {
+  const [loading, setLoading] = useState(false);
 
-  handleDownload = async () => {
+  const handleDownload = async () => {
     try {
-      this.setState({ loading: true });
+      setLoading(true);
 
-      // Fetch data from your API endpoint
-      const response = await fetch('https://www.boredapi.com/api/activity');
+      // Replace 'your-file-url' with the actual URL of the file you want to download
+      const fileUrl = 'https://drive.google.com/file/d/1RjQQjBqSUGzEms5aq6EPlGAqQCqVnsny/view?usp=sharing';
+      const response = await fetch(fileUrl);
       const data = await response.blob();
-
-      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
 
       // Create a link element and trigger a click on it to initiate the download
       const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = 'yourFileName.xlsx'; // Specify the desired file name
+      link.href = URL.createObjectURL(data);
+
+      // Extract the filename from the URL or use a default name
+      const filename = fileUrl.substring(fileUrl.lastIndexOf('/') + 1) || 'downloadedFile';
+
+      link.download = filename;
       link.click();
 
-      // Cleanup by revoking the object URL
       URL.revokeObjectURL(link.href);
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  render() {
-    const { loading } = this.state;
-
-    return (
-      <div>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={this.handleDownload}
-          disabled={loading}
-        >
-          {loading ? 'Downloading...' : 'Download XLSX'}
-        </Button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleDownload}
+        disabled={loading}
+        sx={{ backgroundColor: 'var(--button-bg-color)', color: 'var(--button-text-color)' }}
+      >
+        {loading ? 'Downloading...' : 'Download XLSX'}
+      </Button>
+    </div>
+  );
+};
 
 export default XLSXDownload;
